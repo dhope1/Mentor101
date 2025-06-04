393935038d2df72e1a14722ccab4e8f9b6bb6258
dhope1 <davidhopekavuma@gmail.com>
2025-06-04 11:45:39 +0300

    reverse modified

---
+++ reverse.py
@@ -0,0 +1,26 @@
+#Memory jog on lists first
+a__list = ["clone wars", "Episode 2", "The Mandalorian"]
+#print(a__list)
+empty_list = []
+#print(a__list[0])
+#print(a__list[-1])
+#NOTE Strings are mutable; can be modified after creation
+#[0] = "Episode 1"
+#print(a__list)
+#a__list.append("episode 3") #append takes item and adds it at the end of the list
+#print(a__list)
+#a__list.insert(1, "episode 1") # Insert takes an index and item to be placed at that index
+#print(a__list)
+#a__list.remove("Episode 2") #takes item that it then removes
+#print(a__list)
+#del a__list[2] #del keyword deletes item at specified index
+#print(a__list)
+#a__list.pop() # takes and index as parameter and removes item at that specified index, but if called without an index specified, by default, it removes last item in the list
+#print(a__list)
+#print(a__list[0:1]) #slicing; gives items from start_index to stop index_minus_one
+# a = len(a__list)#returns length of the list
+# b = a__list.sort() # sorts 
+# c = a__list.reverse() #reverses in place
+# d = a__list.count("clone wars") #counts occurrences of a given item
+# e = a__list.index('clone wars') #returns index of specified item
+# print(a, b, c, d, e)
